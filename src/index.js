f1c1539efd52b35fc06d1331e41ae22f5f718352
vincentmhhon <vincentmhhon@gmail.com>
2017-09-04 04:09:21 -0400

    add reducers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
+import registerServiceWorker from './registerServiceWorker'
+import { createStore } from 'redux'
+import reducer from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+//console.log(store)
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
